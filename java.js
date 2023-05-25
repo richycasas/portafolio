48570fe15cc2237bdf31bfd7d6710bc3831c7ba8
richard casas <richycasas06@gmail.com>
2023-05-25 09:05:30 -0500

    cambios portafolio 25 mayo

---
+++ java.js
@@ -0,0 +1,35 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon. onclick = () => {
+    menuIcon. classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window. scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetheight;
+        let id = sec.getAttribute('id');
+
+
+    if(top >= offset && top < offset + height){
+    navLinks.forEach(links => {
+        links.classList. remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classlist.add('active');
+        });
+        };
+    });
+/** ============== sticky navbar ============ */
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+    
+    menuIcon. classList.remove('bx-x');
+    navbar. classList. remove('active');
+
+};
